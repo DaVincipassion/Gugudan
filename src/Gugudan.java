a921f12378e475ff09e600612a7f80c1cce6a1aa
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-16 17:43:44 +0900

    사칙

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,17 +1,11 @@
 
 public class Gugudan {
 	public static void main(String[] args) {
-		// 2´Ü
-		System.out.println(2 * 1);
-		System.out.println(2 * 2);
-		System.out.println(2 * 3);
-		System.out.println(2 * 4);
-		System.out.println(2 * 5);
-		System.out.println(2 * 6);
-		System.out.println(2 * 7);
-		System.out.println(2 * 8);
-		System.out.println(2 * 8);
-
+		System.out.println("µ¡¼À : " + (3+4));
+		System.out.println("–E¼À : " + (3-4));
+		System.out.println("°ö¼À : "+(3*4));
+		System.out.println("³ª´°¼À :"+ (3/4));
+		
 	}
 
 }
