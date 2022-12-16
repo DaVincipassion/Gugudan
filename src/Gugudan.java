70a84dd09bce184fa9c9e2ac7122044a6f5eac5b
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-16 19:01:06 +0900

    계산기 기본구현

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -3,18 +3,26 @@ import java.util.Scanner;
 public class Gugudan {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		
 		System.out.println("첫 번쨰 입력 값: ");
 		int first = scanner.nextInt();
 		System.out.println(first);
 		
+		int result = first;
+		
+		while(true) {
 		System.out.println("연산 입력 값: ");
 		String symbol = scanner.next();
 		System.out.println(symbol);
 		
+		if("quit".equals(symbol)) {
+			System.out.println("최종 결과 값 "+ result);
+		}
+		
+		
 		System.out.println("두 번쨰 입력 값: ");
 		int second = scanner.nextInt();
 		System.out.println(second);
-		
 		
 		
 		if(symbol.equals("+")) {
@@ -33,5 +41,6 @@ public class Gugudan {
 	    System.out.println("사칙연산 기호가 아님");
 	    }
 	}
+}
 }
 
