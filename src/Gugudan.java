64009eef03789426793b39801e85b49d772fd122
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-16 17:58:54 +0900

    사칙연산 스캐너

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-		System.out.println("µ¡¼À : " + (3+4));
-		System.out.println("–E¼À : " + (3-4));
-		System.out.println("°ö¼À : "+(3*4));
-		System.out.println("³ª´°¼À :"+ (3/4));
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ã¹ ¹ø¤Š ÀÔ·Â °ª: ");
+		int first = scanner.nextInt();
+		System.out.println(first);
 		
+		System.out.println("µÎ ¹ø¤Š ÀÔ·Â °ª: ");
+		int second = scanner.nextInt();
+		System.out.println(second);
+		
+		System.out.println("°ªÀÇ ÇÕ" +(first + second));
+		System.out.println("°ªÀÇ –E¼À" +(first - second));
+		System.out.println("°ªÀÇ °ö¼À" +(first * second));
+		System.out.println("°ªÀÇ ³ª´°¼À" +(first / second));
 	}
 
 }
