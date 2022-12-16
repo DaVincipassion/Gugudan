faf852d47a38d637b416b326c61598bf86d3fce9
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-16 18:44:22 +0900

    사칙연산 3개입력

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -7,14 +7,31 @@ public class Gugudan {
 		int first = scanner.nextInt();
 		System.out.println(first);
 		
+		System.out.println("연산 입력 값: ");
+		String symbol = scanner.next();
+		System.out.println(symbol);
+		
 		System.out.println("두 번쨰 입력 값: ");
 		int second = scanner.nextInt();
 		System.out.println(second);
 		
+		
+		
+		if(symbol.equals("+")) {
 		System.out.println("값의 합" +(first + second));
+		}
+		else if(symbol.equals("-")) {
 		System.out.println("값의 뺼셈" +(first - second));
+		}
+		else if(symbol.equals("*")) {
 		System.out.println("값의 곱셈" +(first * second));
+		}
+		else if(symbol.equals("/")) {
 		System.out.println("값의 나눗셈" +(first / second));
+		}
+		else {
+	    System.out.println("사칙연산 기호가 아님");
+	    }
 	}
-
 }
+
